7a1a647e734fcde7af9ab2c6e9970d0fa2040ffa
jaehyuk <jaehyuk7722@naver.com>
2023-12-07 16:16:32 +0900

    init Project

---
+++ src/test/java/com/jh/homeutils/HomeUtilsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jh.homeutils;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeUtilsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
